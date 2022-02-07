abee78c8aabc8a7e7714976134807f5d00c4948b
Storm <61149527+yannicfreson@users.noreply.github.com>
2022-02-07 12:51:52 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,14 +1,15 @@
 const ks = require("node-key-sender");
-const h = 3;
-const m = 15;
-const s = 0;
+const h = 3; // hours until
+const m = 15; // minutes until
+const s = 0; // seconds until
+const msg = ""; // message to send
 
 console.log(
   `Firing at ${new Date(Date.now() + 3600000 * h + 60000 * m + 1000 * s)}`
 );
 
 setTimeout(() => {
-  ks.sendText("Early Bird 2/7 ;p");
+  ks.sendText(msg);
   setTimeout(() => {
     ks.sendKey("enter");
     setTimeout(() => {
